e47777048fd0614c82509e3e62b1d54af572cb59
Nathan Juett <nathanjuett5@gmail.com>
2025-05-18 15:18:54 -0400

    Create CSVs

--- src/VRM/main.py
+++ src/VRM/main.py
@@ -3,6 +3,7 @@ import json
 from dotenv import load_dotenv
 import os
 import datetime
+import csv
 
 def login():
     loginurl = "https://vrmapi.victronenergy.com/v2/auth/login"
@@ -43,21 +44,40 @@ def get_installs(loginobj):
     installs = response.json()
     return installs
 
-def get_install_data(loginobj, installid):
+def get_install_data(loginobj, installid, startdate):
     url = f"https://vrmapi.victronenergy.com/v2/installations/{installid}/stats"
 
     headers = {
         "Content-Type": "application/json",
         "x-authorization": f"Bearer {loginobj["token"]}"
     }
-    stdate = datetime.date.today() 
-    stdate -= datetime.timedelta(days=31)
-    timestamp = int(datetime.datetime.combine(stdate, datetime.datetime.min.time()).timestamp())
+    
+    timestamp = int(datetime.datetime.combine(startdate, datetime.datetime.min.time()).timestamp())
     querystring = {"interval":"15mins", "start":str(timestamp)}
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     data = response.json()
     return data
+
+def write_to_csv(data,key,offset):
+    file = open(f'output/{key}_{offset}.csv', 'w')
+    cw = csv.writer(file)
+    c = 0
+
+    for emp in data["records"][key]:
+        # if c == 0:
+
+        #     # Writing headers of CSV file
+        #     h = emp.keys()
+        #     cw.writerow(h)
+        #     c += 1
+
+        # Writing data of CSV file
+        emp[0] = datetime.datetime.fromtimestamp(emp[0]/1000).strftime('%Y-%m-%d %H:%M:%S')
+        cw.writerow(emp)
+
+    file.close()
+
 
 def main():
     print("Hello, VRM!")
@@ -66,12 +86,25 @@ def main():
         loginobj = login()
         get_tokens(loginobj)
         installs = get_installs(loginobj)
-        data = get_install_data(loginobj, installs["records"][0]["idSite"])
+        stdate = datetime.date.today() 
+        stdate -= datetime.timedelta(days=int(os.getenv("VRM_DAYSPAST")))
+        data = get_install_data(loginobj, installs["records"][0]["idSite"], stdate)
         print(json.dumps(data, indent=4))
-        with open("output.json", "w") as outfile:
+        with open("output/output.json", "w") as outfile:
             json.dump(data, outfile, indent=4)
     else:
-        print("Using test data")
+        with open("output.json", "r") as file:
+            data = json.load(file)
+            print("Using test data")
+    print(json.dumps(data, indent=4))
+    write_to_csv(data, "Pdc", stdate.strftime('%Y-%m-%d'))
+    write_to_csv(data, "bs", stdate.strftime('%Y-%m-%d'))
+    write_to_csv(data, "bv", stdate.strftime('%Y-%m-%d'))
+    write_to_csv(data, "total_solar_yield", stdate.strftime('%Y-%m-%d'))
+    write_to_csv(data, "total_consumption", stdate.strftime('%Y-%m-%d'))
+    write_to_csv(data, "grid_history_from", stdate.strftime('%Y-%m-%d'))
+            
+
 
 
 if __name__ == "__main__":
